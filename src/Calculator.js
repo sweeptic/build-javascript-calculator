0138d2fd0b9ddd28c45b3847f306a640c3469fb1
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-13 16:35:38 +0200

    add basic css-grid layout

---
+++ src/Calculator.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import styles from './Calculator.module.css';
+
+const Calculator = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.div1}>div1</div>
+      <div className={styles.div2}>div2</div>
+      <div className={styles.div3}>div3</div>
+      <div className={styles.div4}>div4</div>
+      <div className={styles.div5}>div5</div>
+      <div className={styles.div6}>div6</div>
+      <div className={styles.div7}>div7</div>
+      <div className={styles.div8}>div8</div>
+      <div className={styles.div9}>div9</div>
+      <div className={styles.div10}>div10</div>
+      <div className={styles.div11}>div11</div>
+      <div className={styles.div12}>div12</div>
+      <div className={styles.div13}>div13</div>
+      <div className={styles.div14}>div14</div>
+      <div className={styles.div15}>div15</div>
+      <div className={styles.div16}>div16</div>
+      <div className={styles.div17}>div17</div>
+      <div className={styles.div18}>div18</div>
+    </div>
+  );
+};
+
+export default Calculator;
